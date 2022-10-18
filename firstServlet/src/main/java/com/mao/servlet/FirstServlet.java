936addda6785b4734b3177472243207bc2bcb0c4
liuy <1463004369@qq.com>
2022-10-18 09:20:25 +0800

    2022.10.17 第一个servlet

---
+++ firstServlet/src/main/java/com/mao/servlet/FirstServlet.java
@@ -0,0 +1,24 @@
+package com.mao.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        request.setCharacterEncoding("utf-8");
+        PrintWriter writer = response.getWriter();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
+        writer.print("<p> 这是我的第一个Servlet 服务器时间是:"+ simpleDateFormat.format(new Date()) + "</p>");
+    }
+}
